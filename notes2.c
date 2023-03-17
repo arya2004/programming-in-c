d44f610846522fbbc0f2b8b6963553d04b27f901
arya2004 <arya20j4@gmail.com>
2023-03-17 19:49:55 +0530

    uy

--- notes2.c
+++ notes2.c
@@ -1,1 +1,9 @@
 //Sem 2
+abstract data struct: format/blueprint of data struct upon which operation can be performd
+
+stach:
+push- add to last
+pop- delete last
+isEmpty- true if stach is empty. used before pop to check uderflow
+isFull()- true if stach is full. used before push to check overflow
+peek()- returns top most element of stack
