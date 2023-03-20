938adb0b9b088c1e113f1e2058bfd62ec571349b
arya2004 <arya20j4@gmail.com>
2023-03-20 14:24:57 +0530

    stack func

--- s.c
+++ s.c
@@ -3,12 +3,72 @@
 #include <math.h>
 #include<string.h>
 
+#define MAX 8
+int stack[8];
+int top = -1;
 
 
+int isFull(){
+    if (top == MAX -1)
+    {
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+int isEmpty(){
+    if (top == -1)
+    {
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
 
+int peek(){
+    return stack[top];
+}
+
+
+void push(int x){
+    if (!isFull())
+    {
+        top = top + 1;
+        stack[top] = x;
+    }
+    else{
+        printf("Overflow\n");
+    }
+    
+}
+
+void pop(){
+    if (!isEmpty())
+    {   int x = stack[top];
+        top = top - 1;
+        
+    }
+    else{
+        printf("underflow\n");
+    }
+    
+}
 
 int main(){
-
+    push(10);
+    push(20);
+    push(30);
+        push(10);
+    push(20);
+    push(30);
+        push(10);
+    push(170);
+    push(30);
+    printf("top %i\n", peek());
+    pop();
+    printf("top %i\n", peek());
 
   return 0;
 }
