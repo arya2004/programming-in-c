74afd75c29e30b577b303ca48b043b6052eefa27
arya2004 <arya20j4@gmail.com>
2023-03-30 12:21:42 +0530

    asas

--- emb_notes.ino
+++ emb_notes.ino
@@ -1,6 +1,6 @@
 println(" "); //prints to console
-serial monitor displays final output. Serial.begin(9600) before that. 9600 is baud rate
+serial monitor displays final output. Serial.begin(9600) before that. 9600 is baud rate. it is written in constructor
 
 void setup(){
-
+//this is constructor
 }
