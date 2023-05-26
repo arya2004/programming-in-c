ca2f28899499f493e168aea848994a92c1f80ade
arya2004 <arya20j4@gmail.com>
2023-05-26 23:05:20 +0530

    idk

--- linked_list.h
+++ linked_list.h
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include <math.h>
 #include<string.h>
-
+#include<mongoc.
 struct node
 { 
     int data;
@@ -145,21 +145,22 @@ struct node* drop_list(struct node* head)
 }
 
 
-// static void reverse(struct Node** head_ref)
-// {
-//     struct Node* prev = NULL;
-//     struct Node* current = *head_ref;
-//     struct Node* next = NULL;
-//     while (current != NULL) {
-//         // Store next
-//         next = current->next;
+static void reverse(struct Node** head_ref)
+{
+    struct Node* prev = NULL;
+    struct Node* current = *head_ref;
+    struct Node* next = NULL;
+    while (current != NULL) {
+        // Store next
+        next = current->next;
  
-//         // Reverse current node's pointer
-//         current->next = prev;
+        // Reverse current node's pointer
+        current->next = prev;
  
-//         // Move pointers one position ahead.
-//         prev = current;
-//         current = next;
-//     }
-//     *head_ref = prev;
-// }
+        // Move pointers one position ahead.
+        prev = current;
+        current = next;
+    }
+    *head_ref = prev;
+}
+
